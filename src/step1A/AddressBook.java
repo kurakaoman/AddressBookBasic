b3c9c6326d66eba90ea21659546651b147958237
kurakaoman <kurakaoman@gmail.com>
2017-08-28 23:15:32 +0200

    ok... to z metoda start (..) zadziałało, powrca mi do głównego menue, ale... w zasadzie co che wyskonać operacje to wykrzacza się NULLPointerExeption... podejzewam że to może być coś z sortowaniem , spróbuje usunąć metode sortującą po każdej operacji iwtedy spróbuje je wykonać

--- src/step1A/AddressBook.java
+++ src/step1A/AddressBook.java
@@ -18,6 +18,9 @@ public class AddressBook {
         Scanner czytnik = new Scanner(System.in);
         ContactList contactList = new ContactList();
 
+        start(czytnik,contactList);
+    }
+    public void start(Scanner czytnik, ContactList contactList){
         System.out.println("Witaj w multimedialnej książce adresowej Marcina Szpaka Addres book" +
         "wydaj dyspozyje:" +
         System.lineSeparator() + "1) wyświetl listę kontaktów" +
@@ -33,11 +36,11 @@ public class AddressBook {
             if(!scan.equals("1") && !scan.equals("2") && !scan.equals("3") && !scan.equals("4") && !scan.equals("5") && !scan.equals("6")) {
                 System.out.println("nie prawidłowy numer akcji!" +
                 System.lineSeparator() + "sprubój jeszcze raz");
-                run();
+                start(czytnik,contactList);
             }
             if(scan.equals("1")){
                 contactList.showContactList();
-                run();
+                start(czytnik,contactList);
                 //System.out.println("Tu masz zawołać metodę która wyświetli listę w odpowiedni sposób");
             }
             if(scan.equals("2")) {
@@ -48,7 +51,7 @@ public class AddressBook {
                 System.out.println("Podaj Nazwisko kontaktu:" + System.lineSeparator());
                 String surname = czytnik.nextLine();
                 contactList.showContact(poz, name, surname);
-                run();
+                start(czytnik,contactList);
                 //System.out.println("Tu masz zawołać metodę która wyświetli wybrany kontakt ");
             }
             if(scan.equals("3")) {
@@ -68,7 +71,7 @@ public class AddressBook {
                 System.out.println("Podaj nr telefonu:" + System.lineSeparator());
                 String phoneNumber = czytnik.nextLine();
                 contactList.addContact(name, surname, country, postalCode, city, street, phoneNumber);
-                run();
+                start(czytnik,contactList);
                 //System.out.println("Tu masz zawołać metodę która dopisze nowy kontakt do listy i wyświetli Urzytkownik XXX został dopisany do listy ");
             }
             if(scan.equals("4")) {
@@ -79,7 +82,7 @@ public class AddressBook {
                 System.out.println("Podaj Nazwisko kontaktu:" + System.lineSeparator());
                 String surname = czytnik.nextLine();
                 contactList.erazeContact(poz, name, surname);
-                run();
+                start(czytnik,contactList);
                 //System.out.println("Tu masz zawołać metodę która usunie wybrany kontakt z listy i wyświetli Urzytkownik XXX został Usunięty ");
             }
             if(scan.equals("5")) {
@@ -101,7 +104,7 @@ public class AddressBook {
                 System.out.println("Podaj nr telefonu:" + System.lineSeparator());
                 String phoneNumber = czytnik.nextLine();
                 contactList.editContact(poz, name, surname, country, postalCode, city, street, phoneNumber);
-                run();
+                start(czytnik,contactList);
                 // Czy on napewno bierze metode edit ze stepa 1 A ??
                 //System.out.println("Tu masz zawołać metodę która edytuje wybrany kontakt i wyświetli Urzytkownik XXX został zmodyfikowany ");
             }
@@ -111,11 +114,11 @@ public class AddressBook {
         } catch(InputMismatchException ime) {
             System.out.println("Wprowadziłeś niepoprawną wartość. należy wprowadzić cyfrę od 1 do 5 odpowiadającą wybranej akcji" +
             System.lineSeparator() + "sprubój ponownie");
-            run();
+            start(czytnik,contactList);
         } catch(NullPointerException npe) {
             System.out.println("nie ma takiego" +
                     System.lineSeparator() + "sprubój ponownie");
-            run();
+            start(czytnik,contactList);
         }
     }
 }
